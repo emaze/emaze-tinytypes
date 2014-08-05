8cdcb074387e5538457c3cdd7ab48816d9bca1c2
Roberto Ferranti <rf@emaze.net>
2014-08-05 13:19:10 +0200

    fix: key serializers/deserializers

--- src/main/java/net/emaze/tinytypes/JacksonTinyTypesModule.java
+++ src/main/java/net/emaze/tinytypes/JacksonTinyTypesModule.java
@@ -11,6 +11,7 @@ import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.KeyDeserializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.deser.Deserializers;
 import com.fasterxml.jackson.databind.module.SimpleModule;
@@ -28,9 +29,9 @@ public class JacksonTinyTypesModule extends SimpleModule {
         this.addSerializer(LongTinyType.class, new LongTinyTypeSerializer());
         this.addSerializer(IntTinyType.class, new IntTinyTypeSerializer());
         this.addSerializer(StringTinyType.class, new StringTinyTypeSerializer());
-        this.addKeySerializer(LongTinyType.class, new LongTinyTypeSerializer());
-        this.addKeySerializer(IntTinyType.class, new IntTinyTypeSerializer());
-        this.addKeySerializer(StringTinyType.class, new StringTinyTypeSerializer());
+        this.addKeySerializer(LongTinyType.class, new LongTinyTypeKeySerializer());
+        this.addKeySerializer(IntTinyType.class, new IntTinyTypeKeySerializer());
+        this.addKeySerializer(StringTinyType.class, new StringTinyTypeKeySerializer());
     }
 
     @Override
@@ -53,6 +54,19 @@ public class JacksonTinyTypesModule extends SimpleModule {
                 return null;
             }
         });
+        context.addKeyDeserializers((type, config, beanDesc) -> {
+            final Class<?> candidateTinyType = type.getRawClass();
+            if (LongTinyType.class.isAssignableFrom(candidateTinyType)) {
+                return new TinyTypesKeyDeserializer(candidateTinyType, long.class);
+            }
+            if (IntTinyType.class.isAssignableFrom(candidateTinyType)) {
+                return new TinyTypesKeyDeserializer(candidateTinyType, int.class);
+            }
+            if (StringTinyType.class.isAssignableFrom(candidateTinyType)) {
+                return new TinyTypesKeyDeserializer(candidateTinyType, String.class);
+            }
+            return null;
+        });
     }
 
     public static class TinyTypesDeserializer extends JsonDeserializer<Object> {
@@ -71,7 +85,26 @@ public class JacksonTinyTypesModule extends SimpleModule {
             return TinyTypesReflector.create(tinyType, ctor, jp.readValueAs(nestedType));
         }
 
-    }    
+    }
+
+    public static class TinyTypesKeyDeserializer extends KeyDeserializer {
+
+        private final Class<?> tinyType;
+        private final Class<?> nestedType;
+
+        public TinyTypesKeyDeserializer(Class<?> tinyType, Class<?> nestedType) {
+            this.tinyType = tinyType;
+            this.nestedType = nestedType;
+        }
+
+        @Override
+        public Object deserializeKey(String key, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+            final Constructor ctor = TinyTypesReflector.ctor(tinyType);
+            Object parsed = nestedType == long.class ? Long.parseLong(key) : (nestedType == int.class ? Integer.parseInt(key) : key);
+            return TinyTypesReflector.create(tinyType, ctor, parsed);
+        }
+
+    }
 
     public static class LongTinyTypeSerializer extends JsonSerializer<LongTinyType> {
 
@@ -80,6 +113,7 @@ public class JacksonTinyTypesModule extends SimpleModule {
             jgen.writeObject(st.value);
         }
     }
+
     public static class IntTinyTypeSerializer extends JsonSerializer<IntTinyType> {
 
         @Override
@@ -87,11 +121,36 @@ public class JacksonTinyTypesModule extends SimpleModule {
             jgen.writeObject(st.value);
         }
     }
+
     public static class StringTinyTypeSerializer extends JsonSerializer<StringTinyType> {
 
         @Override
         public void serialize(StringTinyType st, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
             jgen.writeObject(st.value);
+        }
+    }
+
+    public static class LongTinyTypeKeySerializer extends JsonSerializer<LongTinyType> {
+
+        @Override
+        public void serialize(LongTinyType st, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
+            jgen.writeFieldName(Long.toString(st.value));
+        }
+    }
+
+    public static class IntTinyTypeKeySerializer extends JsonSerializer<IntTinyType> {
+
+        @Override
+        public void serialize(IntTinyType st, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
+            jgen.writeFieldName(Integer.toString(st.value));
+        }
+    }
+
+    public static class StringTinyTypeKeySerializer extends JsonSerializer<StringTinyType> {
+
+        @Override
+        public void serialize(StringTinyType st, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
+            jgen.writeFieldName(st.value);
         }
     }
 }
