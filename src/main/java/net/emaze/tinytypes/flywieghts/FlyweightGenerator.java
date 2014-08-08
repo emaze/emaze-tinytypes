5a61a5f1145347b52b8a7933b7beb0d141c620c1
Roberto Ferranti <rf@emaze.net>
2014-08-08 18:08:33 +0200

    ref: logging

--- src/main/java/net/emaze/tinytypes/flywieghts/FlyweightGenerator.java
+++ src/main/java/net/emaze/tinytypes/flywieghts/FlyweightGenerator.java
@@ -2,14 +2,13 @@ package net.emaze.tinytypes.flywieghts;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javassist.CannotCompileException;
 import javassist.ClassClassPath;
 import javassist.ClassPool;
 import javassist.CtClass;
 import net.emaze.tinytypes.generation.Template;
 import net.emaze.tinytypes.generation.TinyTypesReflector;
+import org.jboss.logging.Logger;
 
 /**
  *
@@ -17,11 +16,14 @@ import net.emaze.tinytypes.generation.TinyTypesReflector;
  */
 public class FlyweightGenerator {
 
+    private static final Logger logger = Logger.getLogger(FlyweightGenerator.class);
+
     public static void createIfNeeded(Class<?> concreteTinyType) throws CannotCompileException {
         final Map<String, String> bindings = TinyTypesReflector.bindings(concreteTinyType);
         if (bindings.get("usefw").equals("false")) {
             return;
         }
+
         final ClassPool pool = ClassPool.getDefault();
         pool.appendClassPath(new ClassClassPath(concreteTinyType));
         final String className = String.format("net.emaze.tinytypes.gen.%sFlyWeight", concreteTinyType.getSimpleName());
@@ -52,13 +54,13 @@ public class FlyweightGenerator {
                 "}")
                 .with(bindings)
                 .asMethodFor(cc);
-
-        cc.debugWriteFile("/tmp");
         try {
-            Class cls = cc.toClass();
+            final Class<?> cls = cc.toClass();
             cls.getMethod("init").invoke(cls);
         } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | SecurityException ex) {
             throw new IllegalStateException(ex);
         }
+        logger.info(String.format("flyweight generated for %s", concreteTinyType.getSimpleName()));
+
     }
 }
